2eb680c0c9d8d695d520f8d9e294244342b6d3b2
Thiago <diasthiago@protonmail.com>
2025-04-09 16:48:12 -0300

    Update BoardConfigSoong.mk

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -63,6 +63,7 @@ SOONG_CONFIG_customNvidiaVars += \
 SOONG_CONFIG_NAMESPACES += customQcomVars
 SOONG_CONFIG_customQcomVars += \
     qti_vibrator_effect_lib \
+    no_fm_firmware \
     qti_vibrator_use_effect_stream \
     supports_extended_compress_format \
     uses_pre_uplink_features_netmgrd
@@ -81,6 +82,7 @@ SOONG_CONFIG_customGlobalVars_gralloc_handle_has_ubwcp_format := $(TARGET_GRALLO
 SOONG_CONFIG_customGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
 SOONG_CONFIG_customGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_customGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
+SOONG_CONFIG_customQcomVars_no_fm_firmware := $(TARGET_QCOM_NO_FM_FIRMWARE)
 SOONG_CONFIG_customNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_customQcomVars_qti_vibrator_use_effect_stream := $(TARGET_QTI_VIBRATOR_USE_EFFECT_STREAM)
 SOONG_CONFIG_customQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
@@ -136,3 +138,18 @@ else
 SOONG_CONFIG_customQcomVars_qcom_display_headers_namespace := $(QCOM_SOONG_NAMESPACE)/display
 endif
 SOONG_CONFIG_customQcomVars_qti_vibrator_effect_lib := $(TARGET_QTI_VIBRATOR_EFFECT_LIB)
+
+# libfmjni
+ ifeq ($(BOARD_HAVE_QCOM_FM),true)
+     PRODUCT_SOONG_NAMESPACES += \
+         vendor/qcom/opensource/libfmjni
+ else ifeq ($(BOARD_HAVE_BCM_FM),true)
+     PRODUCT_SOONG_NAMESPACES += \
+         hardware/broadcom/fm
+ else ifeq ($(BOARD_HAVE_SLSI_FM),true)
+     PRODUCT_SOONG_NAMESPACES += \
+         hardware/samsung_slsi/fm
+ else ifneq ($(BOARD_HAVE_MTK_FM),true)
+     PRODUCT_SOONG_NAMESPACES += \
+         packages/apps/FMRadio/jni/fmr
+ endif
